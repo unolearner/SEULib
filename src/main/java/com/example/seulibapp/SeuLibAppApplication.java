62a67acc2c3c4361d2be4a027aa82aa989d99a30
Whe1ve <zhangruijin666888@gmail.com>
2024-11-30 19:58:38 +0800

    clone files

---
+++ src/main/java/com/example/seulibapp/SeuLibAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.seulibapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeuLibAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeuLibAppApplication.class, args);
+	}
+
+}
