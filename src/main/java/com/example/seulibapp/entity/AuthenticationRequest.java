4ef1b9970aff3d51ae909fdca0f00231a3ca31c1
AzKL42 <93662113+AzKL42@users.noreply.github.com>
2024-12-03 19:54:34 +0800

    JWT权限验证

---
+++ src/main/java/com/example/seulibapp/entity/AuthenticationRequest.java
@@ -0,0 +1,29 @@
+package com.example.app01.entity;
+
+import java.io.Serializable;
+
+public class AuthenticationRequest implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private String username;
+    private String password;
+
+
+    public String getUsername() {
+        return username;
+    }
+
+    public AuthenticationRequest setUsername(String username) {
+        this.username = username;
+        return this;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public AuthenticationRequest setPassword(String password) {
+        this.password = password;
+        return this;
+    }
+}
