8a94b4d8521f8a002dcac745c8ad956dfc77f357
zhongshigui <1466615402@qq.com>
2025-01-02 00:09:41 +0800

    fix: table record creation

--- src/main/java/com/example/seulibapp/sql/create_table_reservation_and_record.sql
+++ src/main/java/com/example/seulibapp/sql/create_table_reservation_and_record.sql
@@ -6,11 +6,11 @@ CREATE TABLE reservation (
 );
 
 CREATE TABLE record (
-    id int AUTO_INCREMENT PRIMARY KEY,        -- 自增主键
-    bid varchar(255) NOT NULL,                          -- 书籍ID，外键
-    user_id int NOT NULL,                     -- 用户ID，外键
-    action_date DATETIME NOT NULL,               -- 动作日期
-    action_type ENUM('BORROW', 'RETURN') NOT NULL, -- 动作类型
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- 创建时间
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP -- 更新时间
+                        id int AUTO_INCREMENT PRIMARY KEY,        -- 自增主键
+                        bid varchar(255) NOT NULL,                          -- 书籍ID，外键
+                        user_id int NOT NULL,                     -- 用户ID，外键
+                        action_date VARCHAR(255) NOT NULL,               -- 动作日期
+                        action_type ENUM('BORROW', 'RETURN','REBORROW') NOT NULL, -- 动作类型
+                        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- 创建时间
+                        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP -- 更新时间
 );
